4f8de6a221df04ec56498e4f7695d719d1137eff
James Decker <decker31@purdue.edu>
2016-10-23 01:00:45 -0400

    Finishing demo
    Former-commit-id: 7b659caa5411ca2fe8c5146fdc02510d00327ca6

--- src/main/scala/Fauxquet/Loader2.scala
+++ src/main/scala/Fauxquet/Loader2.scala
@@ -4,27 +4,6 @@ package main.scala.Fauxquet
   * Created by james on 8/4/16.
   */
 object Loader2 extends App {
-  var sum = 0L
-
-  for (i <- 0 until 1) {
-    val t = time {
-      val file = new FauxquetFile("./resources/customer.parquet")
-      file init()
-    }
-
-    println(s" in $t seconds")
-
-    sum = sum + t
-  }
-
-  println(sum)
-
-  def time[R](block: => R): Long = {
-    val t0 = System.nanoTime()
-    val result = block    // call-by-name
-    val t1 = System.nanoTime()
-    //println("Elapsed time: " + (t1 - t0) + "ns")
-    //result
-    t1 - t0
-  }
+  val file = new FauxquetFile("./resources/lineitem.parquet")
+  file init()
 }
---
+++ test/main/scala/Fauxquet/FauxquetFileTest.scala
@@ -0,0 +1,27 @@
+package main.scala.Fauxquet
+
+import org.scalatest.FunSuite
+
+/**
+  * Created by james on 10/22/16.
+  */
+class FauxquetFileTest extends FunSuite {
+  test("timingTest") {
+    val numRuns = 30
+
+    var sum = 0L
+
+    for (i <- 0 until numRuns) {
+      val t0 = System.nanoTime()
+
+      val fauxquetFile = new FauxquetFile("resources/lineitem.parquet")
+      fauxquetFile init()
+
+      val t1 = System.nanoTime()
+
+      sum += t1 - t0
+    }
+
+    println(s"numRuns took ${sum / 1000000000.0} seconds.")
+  }
+}
