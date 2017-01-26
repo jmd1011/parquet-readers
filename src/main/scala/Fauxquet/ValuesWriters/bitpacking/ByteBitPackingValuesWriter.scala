8723edfb91e970a5c2c5e8ffafbffbe4f99e9986
James Decker <decker31@purdue.edu>
2017-01-26 15:12:09 -0500

    Adding ByteBasedBitPackingEncoder

--- src/main/scala/Fauxquet/ValuesWriters/bitpacking/ByteBitPackingValuesWriter.scala
+++ src/main/scala/Fauxquet/ValuesWriters/bitpacking/ByteBitPackingValuesWriter.scala
@@ -1,12 +1,16 @@
 package main.scala.Fauxquet.ValuesWriters.bitpacking
 
+import main.scala.Fauxquet.Encoders.ByteBasedBitPackingEncoder
 import main.scala.Fauxquet.FauxquetObjs.{BIT_PACKED, Encoding}
 import main.scala.Fauxquet.ValuesWriters.ValuesWriter
 
 /**
   * Created by james on 1/26/17.
   */
-class ByteBitPackingValuesWriter extends ValuesWriter {
+class ByteBitPackingValuesWriter(val bound: Int) extends ValuesWriter {
+  val bitWidth = 32 - Integer.numberOfLeadingZeros(bound)
+  var encoder = new ByteBasedBitPackingEncoder(bitWidth)
+
   override def bufferedSize(): Long = ???
 
   //bunch of other stuff in Parquet -- do we need it?
@@ -14,5 +18,9 @@ class ByteBitPackingValuesWriter extends ValuesWriter {
 
   override def encoding: Encoding = BIT_PACKED
 
+  override def writeInt(i: Int): Unit = this.encoder.writeInt(i)
 
+  override def reset(): Unit = {
+    encoder = new ByteBasedBitPackingEncoder(bitWidth)
+  }
 }
