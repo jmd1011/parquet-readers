1ec0a31bebbf23b0268e70d16e994cf487795a37
James Decker <decker31@purdue.edu>
2017-01-25 21:55:03 -0500

    Created PlainValuesWriter -- not sure if others are needed

---
+++ src/main/scala/Fauxquet/ValuesWriters/plain/PlainValuesWriter.scala
@@ -0,0 +1,35 @@
+package main.scala.Fauxquet.ValuesWriters.plain
+
+import main.scala.Fauxquet.Encoders.LittleEndianEncoder
+import main.scala.Fauxquet.ValuesWriters.ValuesWriter
+import main.scala.Fauxquet.bytes.CapacityByteArrayOutputStream
+
+/**
+  * Created by james on 1/25/17.
+  */
+class PlainValuesWriter(capacityByteArrayOutputStream: CapacityByteArrayOutputStream) extends ValuesWriter {
+  val out = new LittleEndianEncoder(capacityByteArrayOutputStream)
+
+  override def bufferedSize(): Long = capacityByteArrayOutputStream.size
+
+  override def writeByte(value: Int): Unit = throw new Error("PlainValuesWriter.writeByte not allowed (don't ask)")
+
+  override def writeBytes(bytes: Array[Byte]): Unit = {
+    out.writeInt(bytes.length)
+    out.write(bytes)
+  }
+
+  override def writeBoolean(b: Boolean): Unit = out.writeBoolean(b)
+
+  override def writeInt(i: Int): Unit = out.writeInt(i)
+
+  override def writeLong(l: Long): Unit = out.writeLong(l)
+
+  override def writeDouble(d: Double): Unit = out.writeDouble(d)
+
+  override def writeFloat(f: Float): Unit = out.writeFloat(f)
+
+  override def getAllocatedSize: Long = capacityByteArrayOutputStream.bytesAllocated
+
+  def reset() = capacityByteArrayOutputStream.reset()
+}
