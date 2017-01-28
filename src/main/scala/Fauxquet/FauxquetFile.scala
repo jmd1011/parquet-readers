b9eabde91f77ace826f9c89cf639212ed1cf1f82
James Decker <decker31@purdue.edu>
2017-01-28 16:20:08 -0500

    Added _Type code

--- src/main/scala/Fauxquet/FauxquetFile.scala
+++ src/main/scala/Fauxquet/FauxquetFile.scala
@@ -1,5 +1,7 @@
 package main.scala.Fauxquet
 
+import main.scala.Fauxquet.FauxquetObjs.SchemaElement
+
 /**
   * Created by james on 8/5/16.
   */
@@ -15,16 +17,17 @@ class FauxquetFile() {
   def Schema(schema: List[String]) = schema.toVector
 
   var data: Map[String, Vector[Any]] = Map[String, Vector[Any]]() //need to change this when dealing with Record
-  var schema: Schema = _
+  var schema = Vector[SchemaElement]()
   var fields: Fields = _
 
   def read(file: String) = {
     val fauxquetReader = new FauxquetReader(file)
     data = fauxquetReader.read()
+    schema = fauxquetReader.fileMetaData.schema
   }
 
   def write(file: String) = {
     val fauxquetWriter = new FauxquetWriter(file)
-    fauxquetWriter.write(data)
+    fauxquetWriter.write(schema, data)
   }
 }
---
+++ src/main/scala/Fauxquet/Types/MessageType.scala
@@ -0,0 +1,23 @@
+package main.scala.Fauxquet.Types
+
+import main.scala.Fauxquet.FauxquetObjs.{ColumnDescriptor, INT32, TTypeManager}
+
+/**
+  * Created by james on 1/28/17.
+  * Used for Schema (for now)
+  */
+class MessageType(name: String, fields: List[BaseType]) extends GroupType(REPEATED, name, null, fields, null) {
+  def getMaxRepetitionLevel(path: Array[String]): Int = getMaxRepetitionLevel(path, 0) - 1
+  def getMaxDefinitionLevel(path: Array[String]): Int = getMaxDefinitionLevel(path, 0) - 1
+
+  def getType(path: String*): BaseType = getType(path.toArray, 0)
+
+  def getColumnDescription(path: Array[String]): ColumnDescriptor = {
+    val maxR = getMaxRepetitionLevel(path)
+    val maxD = getMaxDefinitionLevel(path)
+
+    new ColumnDescriptor(path, INT32, 0, getMaxRepetitionLevel(path), getMaxDefinitionLevel(path))
+  }
+
+  override def accept(visitor: TypeVisitor): Unit = visitor.visit(this)
+}
